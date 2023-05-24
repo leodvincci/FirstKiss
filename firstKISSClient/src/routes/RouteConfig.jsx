c0eead550e274de6847dfaec4a05eef7e0ec9f81
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-24 12:16:00 -0500

    added routing for logout page

--- firstKISSClient/src/routes/RouteConfig.jsx
+++ firstKISSClient/src/routes/RouteConfig.jsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from "react-router-dom";
 import RegistrationPage from "../pages/RegistrationPage.jsx";
+import LoginPage from "../pages/LoginPage.jsx";
 
 export default function RouteConfig(){
     return(
         <Routes>
             <Route path={"/register"} element={<RegistrationPage/>}/>
+            <Route path={"/login"} element={<LoginPage/>}/>
         </Routes>
 
     )
