f971f998010960f02e3095acb43c2292055d40b4
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-24 00:35:24 -0500

    fixed wrong input ID

--- firstKISSClient/src/pages/RegistrationPage.jsx
+++ firstKISSClient/src/pages/RegistrationPage.jsx
@@ -5,10 +5,11 @@ export default function RegistrationPage(){
     const [userReg, setUserReg] = React.useState({firstName:"",lastName:"",email:"",password:""})
     function handleClick(){
         axios.post('http://127.0.0.1:8000/api/v1/usersignup', {
-            ...userReg
+            userReg
         })
             .then(function (response) {
                 console.log(response);
+                console.log(userReg);
             })
             .catch(function (error) {
                 console.log(error);
@@ -33,8 +34,8 @@ export default function RegistrationPage(){
             <div className={"bg-indigo-500 h-screen flex flex-col justify-center items-center text-2xl w-1/2"}>
                 <h1 className={`text-white text-4xl m-8`}>Let's Get you signed up!</h1>
                 <div className={`flex flex-col justify-between h-1/4`}>
-                    <input onChange={handleForm}  id={"firstName"} className={"rounded-md"} type="text" placeholder={"first name"}/>
-                    <input onChange={handleForm}  id={"lastName"} className={"rounded-md"}  type="text" placeholder={"last name"}/>
+                    <input onChange={handleForm}  id={"first_name"} className={"rounded-md"} type="text" placeholder={"first name"}/>
+                    <input onChange={handleForm}  id={"last_name"} className={"rounded-md"}  type="text" placeholder={"last name"}/>
                     <input onChange={handleForm}  id={"email"} className={"rounded-md"}  type="text" placeholder={"email"}/>
                     <input onChange={handleForm}  id={"password"} className={"rounded-md"} type="password" placeholder={"password"}/>
                     <button onClick={handleClick} className="btn">Register</button>
