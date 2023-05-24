529fcbebdeeefc21e9601490f4582e78d3245f3e
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-23 21:43:17 -0500

    Updated Path name, installed DaisyUI, updated register page

--- firstKISSClient/src/pages/RegistrationPage.jsx
+++ firstKISSClient/src/pages/RegistrationPage.jsx
@@ -1,15 +1,17 @@
 
-export default function LoginPage(){
+export default function RegistrationPage(){
     return(
-        <div className={"flex"}>
+        <div className={"flex flex-row"}>
 
             <div className={"bg-red-500 h-screen flex flex-row justify-center items-center text-9xl w-1/2"}>
                 <h1 >First Kiss</h1>
             </div>
 
-            <div className={"bg-indigo-500 h-screen flex flex-row justify-center items-center text-9xl w-1/2"}>
+            <div className={"bg-indigo-500 h-screen flex flex-col justify-center items-center text-9xl w-1/2"}>
                 <h1 >Let's Get you signed up!</h1>
+                <button className="btn">Register</button>
             </div>
+
 
 
         </div>
--- firstKISSClient/src/routes/RouteConfig.jsx
+++ firstKISSClient/src/routes/RouteConfig.jsx
@@ -1,10 +1,10 @@
 import {Route, Routes} from "react-router-dom";
-import LoginPage from "../pages/LoginPage.jsx";
+import RegistrationPage from "../pages/RegistrationPage.jsx";
 
 export default function RouteConfig(){
     return(
         <Routes>
-            <Route path={"/login"} element={<LoginPage/>}/>
+            <Route path={"/register"} element={<RegistrationPage/>}/>
         </Routes>
 
     )
