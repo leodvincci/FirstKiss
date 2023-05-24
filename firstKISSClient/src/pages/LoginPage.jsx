59450d2ae6b29885d166f8304d1a1d1ca740d4bc
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-24 12:36:36 -0500

    completed login page

--- firstKISSClient/src/pages/LoginPage.jsx
+++ firstKISSClient/src/pages/LoginPage.jsx
@@ -1,17 +1,22 @@
 import fk from "../assets/img.png"
+import logo from "../assets/img_1.png"
 export default function LoginPage(){
     return(
         <div className={"flex flex-row w-screen h-screen"}>
 
-            <div className={"bg-indigo-500 w-full h-full"}>
-                <img className={"mt-[250px]"} src={fk} alt="" width={"800px"}/>
+            <div className={"bg-indigo-500 w-auto h-auto flex flex-col justify-end"}>
+                <img  src={fk} alt="" width={"80%"}/>
             </div>
 
             <div className={"bg-red-400 w-full h-full "}>
                 <div className={"h-full w-full flex flex-col items-center justify-center"}>
-                    <h1>Login</h1>
-                    <input className={"m-2"} type="text"/>
-                    <input type="password"/>
+                    <img src={logo} alt="" width={"30%"}/>
+                    <input className={"m-2 p-2 rounded-md"} type="text" placeholder={"email"}/>
+                    <input className={"m-2 p-2 rounded-md"} type="password" placeholder={"password"}/>
+                    <div className={"m-5"}>
+                        <button className="btn">login</button>
+                    </div>
+
                 </div>
 
             </div>
