13f3464dbb8130ee5c399fae1a19dfe145d267a0
Leo D. Penrose <lpenrose@depaul.edu>
2023-05-26 05:49:59 -0500

    added simpleJWT to DEFAULT_AUTHENTICATION_CLASSES settings

--- firstKissProject/settings.py
+++ firstKissProject/settings.py
@@ -124,3 +124,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
